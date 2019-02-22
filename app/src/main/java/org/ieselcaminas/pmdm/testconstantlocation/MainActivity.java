3547507accecc9d1105b030803e67fb6bfbc8aec
Ferran <vidalferran46@gmail.com>
2019-02-22 16:06:45 +0100

    Initial commit

---
+++ app/src/main/java/org/ieselcaminas/pmdm/testconstantlocation/MainActivity.java
@@ -0,0 +1,97 @@
+package org.ieselcaminas.pmdm.testconstantlocation;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mLatitudeTextView, mLongitudeTextView;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private static final int MY_LOCATION_REQUEST_CODE = 1234;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLatitudeTextView = findViewById(R.id.textView1);
+        mLongitudeTextView = findViewById(R.id.textView2);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    mCurrentLocation = location;
+                    mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+                    mLatitudeTextView.setText(String.valueOf(mCurrentLocation.getLatitude()));
+                    mLongitudeTextView.setText(String.valueOf(mCurrentLocation.getLongitude()));
+                }
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            startUpdating();
+
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_REQUEST_CODE);
+        }
+
+    }
+
+    private void startUpdating() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback,null);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_LOCATION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startUpdating();
+                } else {
+
+                    Dialog d = new AlertDialog.Builder(MainActivity.this).setTitle("Error").
+                            setMessage("I need GPS permission").create();
+                    d.show();
+                }
+                return;
+            }
+
+        }
+
+    }
+}
